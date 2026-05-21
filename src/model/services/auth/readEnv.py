a10b0f2ef4957f9190f255ebe17b81bb016e4cb9
pedro11065 <pedrohenriquesilvaquixabeira@gmail.com>
2026-05-21 17:06:05 -0300

    Mudança na localização do arquivo .env para "etc/secrets/.env".

--- src/model/services/auth/readEnv.py
+++ src/model/services/auth/readEnv.py
@@ -1,6 +1,6 @@
 import os, dotenv
 
-def readEnv(env_path=".env", override=True):
+def readEnv(env_path="etc/secrets/.env", override=True):
 
     dotenv.load_dotenv(dotenv_path=str(env_path), override=override)
     parsed = dotenv.dotenv_values(dotenv_path=str(env_path))
